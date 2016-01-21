fbeaabec224cd9a539423dedea3ecc9bd48c8555
tocjent <bobrowicz.michal@gmail.com>
2016-01-21 17:04:44 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+* zainstalowac nodejs
+* sudo npm install -g bower
+* sudo npm install -g live-server
+* git clone https://github.com/woqlsky/app_store.git
+* cd app_store
+* bower install
+* live-server .
--- src/showAction.js
+++ src/showAction.js
@@ -4,6 +4,7 @@ var showAction = function (id) {
         var application = listData
             .find((listDataElement) => listDataElement.id == id);
         if(typeof application !== 'undefined') {
+
         } else {
             app.append($('<h3>Application with id ' + id + ' was not found.</h3>'));
         }
